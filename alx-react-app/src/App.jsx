67e47f4a9b27594d634cef16c69e366969f88131
maame-gigi <159448426+maame-gigi@users.noreply.github.com>
2025-09-03 08:29:27 +0000

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './components/WelcomeMessage
+import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
